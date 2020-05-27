2ec39d0a1829678a009f3b1fc08aa450be77b139
CT <s310056@hotmail.com>
2020-05-27 23:35:43 +0800

    2

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ extra_compile_args = []
 if ccompiler.get_default_compiler() == "msvc":
     extra_compile_args.append("/utf-8")
 else:
-    extra_compile_args.append('-std=c++11')
+    extra_compile_args.append('-std= c++_static')
 
 
 sxtwl_module = setuptools.Extension('_sxtwl',
